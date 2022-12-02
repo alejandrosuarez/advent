a72753cfd0ecc7ff90d49f15553176a0fb408daf
thejchap <commonmodestudio@gmail.com>
2022-12-01 22:35:08 -0700

    2022 day 2

--- advent/year_2022/day_02.py
+++ advent/year_2022/day_02.py
@@ -1,22 +1,123 @@
 """
+--- Day 2: Rock Paper Scissors ---
+The Elves begin to set up camp on the beach. To decide whose tent gets to be closest to the snack storage,
+a giant Rock Paper Scissors tournament is already in progress.
+
+Rock Paper Scissors is a game between two players. Each game contains many rounds; in each round,
+the players each simultaneously choose one of Rock, Paper, or Scissors using a hand shape.
+Then, a winner for that round is selected: Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock.
+If both players choose the same shape, the round instead ends in a draw.
+
+Appreciative of your help yesterday, one Elf gives you an encrypted strategy guide (your puzzle input)
+that they say will be sure to help you win. "The first column is what your opponent is going to
+play: A for Rock, B for Paper, and C for Scissors. The second column--" Suddenly,
+the Elf is called away to help with someone's tent.
+
+The second column, you reason, must be what you should play in response: X for Rock, Y for Paper, and
+Z for Scissors. Winning every time would be suspicious, so the responses must have been carefully chosen.
+
+The winner of the whole tournament is the player with the highest score. Your total score is the sum of
+your scores for each round. The score for a single round is the score for the shape you selected
+(1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round
+(0 if you lost, 3 if the round was a draw, and 6 if you won).
+
+Since you can't be sure if the Elf is trying to help you or trick you, you should calculate the score
+you would get if you were to follow the strategy guide.
+
+For example, suppose you were given the following strategy guide:
+
+A Y
+B X
+C Z
+This strategy guide predicts and recommends the following:
+
+In the first round, your opponent will choose Rock (A), and you should choose Paper (Y).
+This ends in a win for you with a score of 8 (2 because you chose Paper + 6 because you won).
+In the second round, your opponent will choose Paper (B), and you should choose Rock (X).
+This ends in a loss for you with a score of 1 (1 + 0).
+The third round is a draw with both players choosing Scissors, giving you a score of 3 + 3 = 6.
+In this example, if you were to follow the strategy guide, you would get a total score of 15 (8 + 1 + 6).
+
+What would your total score be if everything goes exactly according to your strategy guide?
+
+Your puzzle answer was 13052.
+
+--- Part Two ---
+The Elf finishes helping with the tent and sneaks back over to you.
+"Anyway, the second column says how the round needs to end: X means you need to lose,
+Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure out what shape
+to choose so the round ends as indicated. The example above now goes like this:
+
+In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y),
+so you also choose Rock. This gives you a score of 1 + 3 = 4.
+In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X)
+with a score of 1 + 0 = 1.
+In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+Following the Elf's instructions for the second column, what would your total score be if everything
+goes exactly according to your strategy guide?
+
+Your puzzle answer was 13693.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
+At this point, you should return to your Advent calendar and try another puzzle.
+
+If you still want to see it, you can get your puzzle input.
+
+You can also [Share] this puzzle.
 """
 
 from advent.tools import *
 
+PTS = {"X": 1, "Y": 2, "Z": 3}
+WINS = {"AY", "BZ", "CX"}
+TIES = {"AX", "BY", "CZ"}
+WINS2 = {"A": "Y", "B": "Z", "C": "X"}
+TIES2 = {"A": "X", "B": "Y", "C": "Z"}
+LOSSES = {"A": "Z", "B": "X", "C": "Y"}
+
 
 def _pt1(lines):
-    pass
+    total = 0
+    for t, m in lines:
+        me = PTS[m]
+        win = 0
+        if t + m in WINS:
+            win = 6
+        elif t + m in TIES:
+            win = 3
+        score = win + me
+        total += score
+    return total
 
 
 def _pt2(lines):
-    pass
+    ls = []
+    for t, m in lines:
+        if m == "Y":
+            ls.append([t, TIES2[t]])
+        elif m == "Z":
+            ls.append([t, WINS2[t]])
+        else:
+            ls.append([t, LOSSES[t]])
+    return _pt1(ls)
 
 
-TEST = """
+TEST = """A Y
+B X
+C Z
 """
-ANSWERS = None
+ANSWERS = [15, 13052, 12, 13693]
 
 
 def main():
-    return afs.input_lines(tests=[TEST], parts=[_pt1], run_input=False)
-
+    return afs.input_lines(
+        tests=[TEST],
+        parts=[_pt1, _pt2],
+        run_input=True,
+        transform_line=lambda l: l.split(" "),
+    )
